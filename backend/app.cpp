80a9f7592c52ece528981edf7868c23d767c1912
Spoorthi-pixel <spoorthi.05sh@gmail.com>
2026-02-26 19:26:52 +0530

    Modified app.cpp

--- backend/app.cpp
+++ backend/app.cpp
@@ -42,11 +42,16 @@ int main() {
         int client_fd = accept(server_fd, NULL, NULL);
         if (client_fd < 0) continue;
         
-        // Simple HTTP response
+        // Build response body first
+        std::string body = "Served by backend: " + std::string(hostname) + "\n";
+
+        // Proper HTTP response with Content-Length
         std::string response = "HTTP/1.1 200 OK\r\n";
         response += "Content-Type: text/plain\r\n";
-        response += "Connection: close\r\n\r\n";
-        response += "Served by backend: " + std::string(hostname) + "\n";
+        response += "Content-Length: " + std::to_string(body.length()) + "\r\n";
+        response += "Connection: close\r\n";
+        response += "\r\n";
+        response += body;
         
         send(client_fd, response.c_str(), response.length(), 0);
         close(client_fd);
